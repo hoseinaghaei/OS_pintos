b0cb78ba65380ed754bb3da693fdd834aef40c47
Dutchman <amk_amir82@yahoo.com>
2023-05-21 16:42:32 +0000

    edit Makefile to run cache

--- src/filesys/cache.h
+++ src/filesys/cache.h
@@ -5,13 +5,13 @@
 #define CACHE_SIZE 64
 
 typedef struct cache_block_item
-    {
-        bool valid;
-        bool dirty;
-        char data[BLOCK_SECTOR_SIZE];
-        struct list_elem cache_elem;
-        struct lock cache_item_lock;
-    } cache_block_item;
+{
+    bool valid;
+    bool dirty;
+    char data[BLOCK_SECTOR_SIZE];
+    struct list_elem cache_elem;
+    struct lock cache_item_lock;
+} cache_block_item;
 
 cache_block_item cache[CACHE_SIZE];
 struct list cache_used_list;
